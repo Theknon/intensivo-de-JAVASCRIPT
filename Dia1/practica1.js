47ca83a984c1662825b7974a9edb71403b5afaf3
unknown <pepineda12@misena.edu.co>
2020-04-11 17:46:20 -0500

    f

--- Dia1/practica1.js
+++ Dia1/practica1.js
@@ -1,8 +1,17 @@
-function saludo(){
-    document.write("Hola bebe")
+var peso = parseInt(prompt("Cual es tu peso"));
+var planeta = prompt("Cual planeta quieres? jupiter o marte");
+var g_tierra = 9.8;
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+var pesoFinal;
+pesoFinal = parseInt(pesoFinal);
+
+if(planeta == "marte"){
+    pesoFinal = peso  * g_marte / g_tierra;
+    document.write("Tu peso en marte es de: " + pesoFinal)
+}else if(planeta == "jupiter"){
+    pesoFinal = peso  * g_jupiter / g_tierra;
+    document.write("Tu peso en jupiter es de: " + pesoFinal)
+}else{
+    document.write("Tu peso es de 878787827812828278 loca");
 }
-
-
-var uno = 25
-var dos =15
-document.write(uno > dos ? "es mayor" : "es menor"); //operador ternario
