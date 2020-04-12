726723337e44dfce1c6242b2076f24f26a7d2060
unknown <pepineda12@misena.edu.co>
2020-04-12 12:08:44 -0500

    Se trabajan funciones

--- Dia1/dibujo.js
+++ Dia1/dibujo.js
@@ -1,9 +1,15 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+dibujarLinea("red", 100, 50, 50, 200);
+dibujarLinea("Brue", 0, 0, 20, 300)
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
