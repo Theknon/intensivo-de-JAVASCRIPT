992898df3414d6bcac025cf8417776b6be65f294
unknown <pepineda12@misena.edu.co>
2020-04-12 15:50:52 -0500

    Se crea practica con ciclo for y ciclo While

--- Dia1/while.js
+++ Dia1/while.js
@@ -1,20 +1,27 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+
 var lineas = 30; 
 var l = 0;
-var yi, xf;
-var colores = "blue";
+var yi, xf, xi, yf;
+var colores = "pink";
 
 while(l < lineas)
 {
     yi = 10 * l;
-    xf = 10 * (l + 1);
+    xf = 10 * l;
+    xi = 10 * l;
+    yf = 10 * l;
     dibujarLinea(colores, 0, yi, xf, 300);
+    dibujarLinea("blue", xi, 0, 300, yf);
+    dibujarLinea("pink", xi, 0, )
     l++;
 }
 
-dibujarLinea("red", 1, 1, 1, 300);
+dibujarLinea("red", 1, 1, 1, 299);
 dibujarLinea("red", 1, 299, 299, 299);
+dibujarLinea("red", 1, 1, 299, 1);
+dibujarLinea("red", 299, 299, 299, 1 );
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
